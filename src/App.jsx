726d8e17608eeb7f2bdd0f6bc728d9833a25bc7b
lavnyasharma <lavanya2210sharma@gmail.com>
2024-05-13 13:19:09 +0530

    nptel

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class CertificateButton extends React.Component {
+  componentDidMount() {
+    window.onbeforeunload = function () {
+      window.history.forward();
+    };
+
+    // You can use this to get the certificate path if needed
+    const certPath = window.location.pathname;
+  }
+
+  render() {
+    return (
+      <div className="nptel-content">
+        <button style={{ 
+          color: 'white', 
+          backgroundColor: '#3070bf', 
+          padding: '10px 20px', 
+          borderRadius: '8px', 
+          textDecoration: 'none', 
+          display: 'inline-block', 
+          width: 'auto', 
+          position: 'absolute', 
+          top: '40%', 
+          left: '50%', 
+          transform: 'translate(-50%, -50%)', 
+          border: 'none' 
+        }}>
+          <a href="./NPTEL23CE66S738100507.png" target="_blank" rel="noreferrer" style={{ color: 'white', textDecoration: 'none' }}>Course Certificate</a>
+        </button>
+      </div>
+    );
+  }
+}
+
+export default CertificateButton;
